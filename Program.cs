6488cab5e56989a0912b403dcc5473140af96f96
GerardAlmirall <148850439+GerardAlmirall@users.noreply.github.com>
2024-05-03 00:21:06 +0200

    installer y últimas configuraciones

--- Program.cs
+++ Program.cs
@@ -1,24 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using FP._059_NAGASystems_Prod3.Data;
+using System.Diagnostics; // Importante para utilizar Process
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Agrega el contexto de la base de datos
 builder.Services.AddDbContext<FP_059_NAGASystems_Prod3Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("FP_059_NAGASystems_Prod3Context") ?? throw new InvalidOperationException("Connection string 'FP_059_NAGASystems_Prod3Context' not found.")));
 
-// Add services to the container.
+// Agrega servicios al contenedor.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configura el pipeline de solicitudes HTTP.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // El valor predeterminado de HSTS es de 30 días. Puedes cambiar esto para escenarios de producción, consulta https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -28,5 +31,10 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Lanzar el navegador automáticamente al iniciar la aplicación
+
+
+    Process.Start(new ProcessStartInfo("cmd", $"/c start http://localhost:{builder.Configuration["ApplicationPort"]}") { CreateNoWindow = true });
 
 app.Run();
